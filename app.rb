c5a959d37a97aba6804693ea5db73f8c02fee0e6
Donald Newholm <newholm1@hotmail.com>
2019-12-31 21:33:40 +0000

    Cat form works on index page and cat picture page

--- app.rb
+++ app.rb
@@ -2,19 +2,19 @@ require 'sinatra'
 
 get '/random-cat' do
   @cat_name = ["Tiddles", "Chairman Miaow", "Cheetara"].sample
-  erb(:index)
+  erb :named_cat
 end
 
-get '/named-cat' do
+get '/' do
+  erb :index
+end
+
+post '/named-cat' do
   p params
   @cat_name = params[:name]
-  erb(:index)
+  erb :named_cat
 end
 
 get '/secret' do
   "They were dead alien robots having a dream all along"
-end
-
-get '/bumwag' do
-  "wagging and waggling bums"
 end
