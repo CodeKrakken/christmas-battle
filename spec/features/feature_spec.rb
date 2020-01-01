bcd600e825f6937c1f3afa4639f5b20a2f7a40da
Donald Newholm <newholm1@hotmail.com>
2020-01-01 12:23:34 +0000

    Player 1 can attack Player 2 and receive a confirmation

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -18,3 +18,11 @@ feature 'Hit points' do
     expect(page).to have_content('Bejlo - 60 HP')
   end
 end
+
+feature 'Attacking' do
+  scenario 'Player 1 can attack player 2' do
+    sign_in_and_play
+    click_button('Attack')
+    expect(page).to have_content('Caxul attacks Bejlo!')
+  end
+end
